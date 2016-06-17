a47ce64b6be8f0d467bd2d9102a2956980100c6f
Daniel Miranda Calzadilla <dcalzadilla@uho.edu.cu>
2016-06-17 15:24:40 -0400

    Added MetroStrip and MetroToolbar Controls

---
+++ MetroFramework/Controls/MetroMenuStrip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetroFramework.Controls
+{
+    /// <summary>
+    /// Menustrip for ModernUI-GUIs
+    /// </summary>
+    public class MetroMenuStrip : System.Windows.Forms.MenuStrip
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MetroMenuStrip()
+            : base()
+        {
+            Renderer = new metroToolStripRenderer();
+            Font = MetroUI.Style.BaseFont;
+            ForeColor = MetroUI.Style.ForeColor;
+        }
+
+        /// <summary>
+        /// OnItemAdded-Event we adjust the font and forecolor of this item
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnItemAdded(System.Windows.Forms.ToolStripItemEventArgs e)
+        {
+            base.OnItemAdded(e);
+
+            e.Item.Font = MetroUI.Style.BaseFont;
+            e.Item.ForeColor = MetroUI.Style.ForeColor;
+        }
+    }
+}
